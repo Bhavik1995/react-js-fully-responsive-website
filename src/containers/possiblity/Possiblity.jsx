ebf68626aa0f6dae6d80e176eea0c86dfc7bcbdb
Bhavik <sejpalbhavik25@gmail.com>
2021-11-17 11:29:17 +0530

    Website updated

--- src/containers/possiblity/Possiblity.jsx
+++ src/containers/possiblity/Possiblity.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import possiblityImage from '../../assets/possibility.png';
 import './possiblity.css';
 
 const Possiblity = () => {
     return (
-        <div>
-            Possiblity
+        <div className="gpt3__possiblity section__paddding" id="possiblity">
+                <div className="gpt3__possiblity-image">
+                    <img src={possiblityImage} alt={possiblityImage}/>
+                </div>
+
+                <div className="gpt3__possiblity-content">
+                    <h4>Request Early Access to Get Started</h4>
+
+                    <h1 className="gradient__text">The possibilities are <br /> beyond your imagination.</h1>
+                    <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+                    <h4>Request Early Access to Get Started</h4>
+                </div>
         </div>
     )
 }
